ff69e5016804a3c330e344c23371de81839cfddf
Imminencce <20enler02@gmail.com>
2022-09-05 18:49:22 +0300

    Adding project on GitHub

---
+++ assets/js/main.js
@@ -0,0 +1,97 @@
+$(function() {
+  
+  let sticky = $('.sticky');
+  let contentOffset;
+  let nav_height;
+  
+  if (sticky.length) {
+    
+    if ( sticky.data('offset') ) {
+      contentOffset = sticky.data('offset');
+    }
+    else {
+      contentOffset = sticky.offset().top;
+    }
+    nav_height = sticky.height();
+  }
+  
+  let scrollTop = $(window).scrollTop();
+  let window_height = $(window).height();
+  let doc_height = $(document).height();
+  
+  $(window).bind('resize', function() {
+    scrollTop = $(window).scrollTop();
+    window_height = $(window).height();
+    doc_height = $(document).height();
+    navHeight();
+  });
+  
+  $(window).bind('scroll', function() {
+    stickyNav();
+  });
+  
+  function navHeight() {
+    sticky.css('max-height', window_height + 'px');
+  }
+  
+  function stickyNav() {
+    scrollTop = $(window).scrollTop();
+    if (scrollTop > contentOffset) {
+      sticky.addClass('fixed');
+    }
+    else {
+      sticky.removeClass('fixed');
+    }
+  }
+  
+});
+
+$('document').ready(function() {
+  let nav_height = 70;
+  
+  $("a[data-role='smoothscroll']").click(function(e) {
+    e.preventDefault();
+    
+    let position = $($(this).attr("href")).offset().top - nav_height;
+    
+    $("body, html").animate({
+      scrollTop: position
+    }, 1000 );
+    return false;
+  });
+});
+
+$('document').ready(function() {
+  let backTop = $(".back-to-top");
+  
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 400) {
+      backTop.css('visibility', 'visible');
+    }
+    else if($(document).scrollTop() < 400) {
+      backTop.css('visibility', 'hidden');
+    }
+  });
+  
+  backTop.click(function() {
+    $('html').animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+  });
+});
+
+
+$('document').ready(function() {
+
+  $(window).on('load', function() {
+    $('.loader-container').fadeOut();
+  });
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $('[data-toggle="popover"]').popover();
+
+  new WOW().init();
+});
+
